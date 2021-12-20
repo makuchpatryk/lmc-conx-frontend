11a368ffb8a81d6a201640bf33488240d2d97fca
Pat <patryk@scooploop.com>
2021-12-20 19:35:07 +0100

    add logic and html

--- src/components/FleetManager.vue
+++ src/components/FleetManager.vue
@@ -1,27 +1,77 @@
 <template>
   <div class="fleet-manager">
     <div class="list">
-      here goes list of machines
+      <ul>
+        <li v-for='item in activeMachine' :key="item" @click="showDeets(item)" v-bind:class="{active: isMachineDeetActive(item)}">
+          <div class="make" v-text="item.make"></div>
+          <div class="type" v-text="item.type"></div>
+        </li>
+      </ul>
     </div>
     <div class="details">
-      here goes the details of a selected machine
+      <fleetDetail
+            v-for="machine in activeMachine"
+            :key="machine"
+            :machine="machine" v-show="isMachineDeetActive(machine)"
+        />
+      <span class="no-selection" v-if="!machineDeet">
+          no selection
+      </span>
     </div>    
   </div>
 </template>
 
 <script>
+import machines from '../api/mockApi.js'
+import fleetDetail from './fleetDetail.vue'
 
 export default {
   name: 'FleetManager',
+  components: { fleetDetail },
+  data: function ()
+  {
+    return {
+      'machinesList': machines,
+      'machineDeet': null
+    }
+  },
+  computed: {
+    activeMachine() {
+      return this.machinesList.filter(product => product.active);
+    }
+  },
+  methods: {
+    isMachineDeetActive: function(item)
+    {
+      if(item == this.machineDeet)
+      {
+        return true;
+      }
+      return false;
+    },
+    showDeets: function(item)
+    {
+      if(this.machineDeet == item)
+      {
+        this.machineDeet = null;
+      }
+      else
+      {
+      this.machineDeet = item;
+      }
+    }
+  }
 }
-
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .list, .details {
   display: inline-block;
   margin: 20px;
+  vertical-align: top;
+}
+.details {
+  min-width: 400px;
 }
 
 ul {
@@ -30,7 +80,22 @@ ul {
 }
 
 li {
-  display: inline-block;
+  display: block;
   margin: 0 10px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+li.active {
+  background-color: #ddd;
+}
+.make {
+  font-weight: 600;
+}
+.type {
+  color: #777;
+}
+.no-selection {
+  display: block;
+  padding: 20px;
 }
 </style>
---
+++ src/components/fleetDetail.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="detail">
+      <span class="make">{{ machine.make }}</span>
+      <span class="type">{{ machine.type }}</span>
+      <div class="location" v-show="machine.location">
+        <span class="cord">E <span v-text="machine.location.E"></span></span>
+        <span class="cord">H <span v-text="machine.location.H"></span></span>
+        <span class="cord">N <span v-text="machine.location.N"></span></span>
+      </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'fleetDetail',
+  props: {
+    machine: {
+      type: Object,
+    }
+  },
+}
+
+</script>
+
+<style scoped>
+.detail {
+  display: inline-block;
+  padding: 20px;
+}
+.make {
+  font-weight: 600;
+  display: block;
+}
+.type {
+  color: #777;
+}
+.location {
+  color: #777;
+  margin-top: 10px;
+}
+.location .cord {
+  display: block;
+}
+
+</style>
